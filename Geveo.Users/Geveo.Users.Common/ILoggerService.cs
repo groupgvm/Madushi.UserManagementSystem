10654cc41e1147b08445c14afb385ad4cfc1a18b
Madushi Sarathchandra <madushi@geveo.com>
2023-03-13 17:53:09 +0530

    Implemented logging middleware

---
+++ Geveo.Users/Geveo.Users.Common/ILoggerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geveo.Users.Common
+{
+    public interface ILoggerService
+    {
+        void LogInfo(string message);
+        void LogError(string errorMessage);
+    }
+}
